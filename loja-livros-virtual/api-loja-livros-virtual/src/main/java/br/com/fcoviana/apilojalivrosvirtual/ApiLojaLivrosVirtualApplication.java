f4828d3e3c05eefc053d1f5bfea25b3ecc1bd1c9
Francisco Viana <franciscoviana862@gmail.com>
2025-03-02 09:44:16 -0300

    Initialize api-loja-livros-virtual project with Maven setup and basic application structure

---
+++ loja-livros-virtual/api-loja-livros-virtual/src/main/java/br/com/fcoviana/apilojalivrosvirtual/ApiLojaLivrosVirtualApplication.java
@@ -0,0 +1,13 @@
+package br.com.fcoviana.apilojalivrosvirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaLivrosVirtualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLojaLivrosVirtualApplication.class, args);
+    }
+
+}
---
+++ loja-livros-virtual/api-loja-livros-virtual/src/test/java/br/com/fcoviana/apilojalivrosvirtual/ApiLojaLivrosVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcoviana.apilojalivrosvirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaLivrosVirtualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
